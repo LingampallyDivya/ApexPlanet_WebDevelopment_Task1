9e4044ad96437cea9e69c636d8147d2c8a3a4ae8
LingampallyDivya <lingampallydivya85@gmail.com>
2025-08-21 19:00:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select button by ID
+const button = document.getElementById("alertButton");
+
+// Add click event listener
+button.addEventListener("click", function() {
+  alert("Hello! 🎉 You just clicked the button.");
+});
